73f85faefa8962b39e36183c5ac2e6e4060a3114
Chris Scavello <bideowego@gmail.com>
2015-03-13 13:24:11 -0400

    Add jQuery and Bootstrap 3

--- public_html/install/inc/view.php
+++ public_html/install/inc/view.php
@@ -2,7 +2,13 @@
 <html>
     <head>
         <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
         <title>WegoDB Installer</title>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
     </head>
     <body>
         <h1><?php echo $heading; ?></h1>
